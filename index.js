20ea7aa53f8382b50b6854bf31bc7fcc7b523293
Mitesh Jadav <jadavmitesh.jd@gmail.com>
2021-10-07 12:30:51 +0530

    POST endpoint

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 4000;
+
+//mongoose connnection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:37017/CRMdb', {
+  useNewUrlparser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
